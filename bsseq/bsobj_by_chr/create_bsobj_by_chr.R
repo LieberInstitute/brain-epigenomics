7aaef6a7198e1361c5d325aa3e7e4d00b6aee1e4
lcolladotor <lcollado@jhsph.edu>
2016-12-09 14:55:58 -0500

    Add skeleton scripts for creating BSobj by chr -- currently not reading CHH files

---
+++ bsseq/bsobj_by_chr/create_bsobj_by_chr.R
@@ -0,0 +1,59 @@
+## Required libraries
+library('getopt')
+library('bsseq')
+library('readxl')
+library('BiocParallel')
+library('devtools')
+
+## Specify parameters
+spec <- matrix(c(
+    'chr', 'c', 1, 'character', 'chromosome to subset',
+    'cores', 't', 1, 'integer', 'Number of cores to use',
+	'help' , 'h', 0, 'logical', 'Display help'
+), byrow=TRUE, ncol=5)
+opt <- getopt(spec)
+
+## if help was asked for print a friendly message
+## and exit with a non-zero error code
+if (!is.null(opt$help)) {
+	cat(getopt(spec, usage=TRUE))
+	q(status=1)
+}
+
+# read in cleaned phenotype
+pd <- read_excel(
+    '/users/ajaffe/Lieber/Projects/WGBS/Analysis/WGBS-pd-fixed.xlsx')
+pd <- pd[,1:36] # fix excel issues
+pd <- pd[!is.na(pd$Data.ID), ]
+
+## keep only postnatal
+pd <- pd[pd$Age > 0,]
+
+## all report files
+pd$reportFiles <- file.path(
+    '/dcl01/lieber/WGBS/LIBD_Data/DNAm_Ratios_duplicates_dropped/Reports',
+	pd$Data.ID, paste0(pd$Data.ID,
+    '.concatenated.sorted.duplicatesRemoved.CpG_report.txt'))
+
+## Load the data
+BSobj <- combineList(bplapply(pd$reportFiles, function(input_file) {
+    res <- read.bismark(pd$reportFiles[ii], pd$WGC.ID[ii],
+        strandCollapse=TRUE, fileType = 'cytosineReport')
+    res <- res[seqnames(res) == opt$chr, ]
+    return(res)
+}, BPPARAM = MulticoreParam(opt$cores)))
+
+## add sample names
+sampleNames(BSobj) <- pd$Data.ID
+rownames(pd) <- pd$Data.ID
+
+## append phenotype data
+pData(BSobj) <- DataFrame(pd)
+save(BSobj, file = paste0(opt$chr, '_postNatal_cleaned_CpGonly.rda'))
+
+## Reproducibility information
+print('Reproducibility information:')
+Sys.time()
+proc.time()
+options(width = 120)
+session_info()
---
+++ bsseq/bsobj_by_chr/create_bsobj_by_chr.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+## Usage:
+# sh create_bsobj_by_chr.sh
+
+mkdir -p logs
+
+for chrnum in {1..22} X Y M
+do 
+
+chr="chr${chrnum}"
+SHORT="bsseq_${chr}"
+
+# Construct shell file
+echo "Creating script for chromosome ${chr}"
+cat > .${SHORT}.sh <<EOF
+#!/bin/bash
+#$ -cwd
+#$ -l mem_free=20G,h_vmem=25G
+#$ -N ${SHORT}
+#$ -pe local 8
+#$ -o ./logs/bsseq_by_chr.o.txt
+#$ -e ./logs/bsseq_by_chr.e.txt
+#$ -m e
+
+echo "**** Job starts ****"
+date
+
+Rscript create_bsobj_by_chr.R -c ${chr} -t 8
+
+echo "**** Job ends ****"
+date
+EOF
+    call="qsub .${SHORT}.sh"
+    echo $call
+    $call
+done
