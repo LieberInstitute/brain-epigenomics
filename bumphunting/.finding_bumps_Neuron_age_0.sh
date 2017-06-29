b10369a72b20e4df35f619a263ee429a540f4281
lcolladotor <lcollado@jhsph.edu>
2017-06-29 12:55:38 -0400

    Add plots and update .gitignore files

---
+++ bumphunting/.finding_bumps_Neuron_age_0.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#$ -cwd
+#$ -l mem_free=35G,h_vmem=40G,h_fsize=100G
+#$ -N finding_bumps_Neuron_age_0
+#$ -pe local 4
+#$ -o ./logs/finding_bumps_Neuron_age_0.txt
+#$ -e ./logs/finding_bumps_Neuron_age_0.txt
+#$ -m e
+
+echo "**** Job starts ****"
+date
+
+Rscript find_bumps.R -m age -s Neuron -p 0 -t 4
+
+echo "**** Job ends ****"
+date
---
+++ bumphunting/.finding_bumps_Neuron_cell_5.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#$ -cwd
+#$ -l mem_free=10G,h_vmem=12G,h_fsize=100G
+#$ -N finding_bumps_Neuron_cell_5
+#$ -pe local 8
+#$ -o ./logs/finding_bumps_Neuron_cell_5.txt
+#$ -e ./logs/finding_bumps_Neuron_cell_5.txt
+#$ -m e
+
+echo "**** Job starts ****"
+date
+
+Rscript find_bumps.R -m cell -s Neuron -p 5 -t 8
+
+echo "**** Job ends ****"
+date
