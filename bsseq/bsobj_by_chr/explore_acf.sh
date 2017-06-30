9d50a1eb7b44aa88a0815e4aac299b376d75ed0c
Leonardo Collado Torres <lcollado@jhsph.edu>
2017-06-30 17:34:46 -0400

    Run for chrs 2-20 and X as well

--- bsseq/bsobj_by_chr/explore_acf.sh
+++ bsseq/bsobj_by_chr/explore_acf.sh
@@ -6,8 +6,8 @@
 mkdir -p logs
 
 #for chrnum in {1..22} X Y M
-for chrnum in M Y 21 22
-#for chrnum in {3..20} X
+#for chrnum in M Y 21 22
+for chrnum in {2..20} X
 #for chrnum in {2..22} X Y M
 do 
 
