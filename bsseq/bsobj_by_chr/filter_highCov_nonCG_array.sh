0ca15c15dce2541c69fce43eaf230f6d6a4e96fb
Leonardo Collado Torres <lcollado@jhsph.edu>
2017-07-05 11:54:51 -0400

    Lower mem, fix TASK ID part

--- bsseq/bsobj_by_chr/filter_highCov_nonCG_array.sh
+++ bsseq/bsobj_by_chr/filter_highCov_nonCG_array.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #$ -cwd
-#$ -l mem_free=130G,h_vmem=130G,h_fsize=100G
+#$ -l mem_free=40G,h_vmem=40G,h_fsize=100G
 #$ -N nonCG_highCov_array
-#$ -o ./logs/nonCG_highCov.${TASK_ID}.txt
-#$ -e ./logs/nonCG_highCov.${TASK_ID}.txt
+#$ -o ./logs/nonCG_highCov.$TASK_ID.txt
+#$ -e ./logs/nonCG_highCov.$TASK_ID.txt
 #$ -m e
 #$ -t 1-25
 
