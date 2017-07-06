b4fb76b027c2e44d86d7faa969d9ba4cf55c3b82
Leonardo Collado Torres <lcollado@jhsph.edu>
2017-07-05 22:18:00 -0400

    Re-run with higher mem

--- bsseq/bsobj_by_chr/explore_nonCG_highCov.sh
+++ bsseq/bsobj_by_chr/explore_nonCG_highCov.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #$ -cwd
-#$ -l bluejay,mem_free=40G,h_vmem=40G,h_fsize=100G
+#$ -l bluejay,mem_free=100G,h_vmem=100G,h_fsize=100G
 #$ -N explore_nonCG_highCov
 #$ -o ./logs/explore_nonCG_highCov.txt
 #$ -e ./logs/explore_nonCG_highCov.txt
