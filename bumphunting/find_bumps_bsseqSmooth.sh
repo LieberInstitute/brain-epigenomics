2640a69c83784eb65cecb1983eb4c643989d340b
Leonardo Collado Torres <lcollado@jhsph.edu>
2017-07-14 21:02:06 -0400

    Add scripts for running bsseq::BSmooth() instead of smoothing with bumphunter

---
+++ bumphunting/find_bumps_bsseqSmooth.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+## Usage:
+# sh find_bumps_bsseqSmooth.sh
+
+CORES=4
+mkdir -p logs
+
+for cell in Neuron
+do
+    for model in cell age interaction
+    do
+        for permutations in 0
+        do
+
+SHORT="finding_bumps_${cell}_${model}_${permutations}"
+
+# Construct shell file
+echo "Creating script for chromosome Glia + ${cell} using model ${model} with ${permutations} permutations"
+
+cat > .${SHORT}.sh <<EOF
+#!/bin/bash
+#$ -cwd
+#$ -l bluejay,mem_free=35G,h_vmem=40G,h_fsize=100G
+#$ -N ${SHORT}
+#$ -pe local ${CORES}
+#$ -o ./logs/${SHORT}.txt
+#$ -e ./logs/${SHORT}.txt
+#$ -m e
+
+echo "**** Job starts ****"
+date
+
+Rscript find_bumps_bsseqSmooth.R -m ${model} -s ${cell} -p ${permutations} -t ${CORES}
+
+echo "**** Job ends ****"
+date
+EOF
+
+call="qsub .${SHORT}.sh"
+echo $call
+$call
+        done
+    done
+done
---
+++ bumphunting/find_bumps_bsseqSmoth.R
@@ -0,0 +1,130 @@
+library('bsseq')
+library('bumphunter')
+library('devtools')
+library('getopt')
+library('doParallel')
+
+## Specify parameters
+spec <- matrix(c(
+	'model', 'm', 1, 'character', 'Either: cell, age or interaction',
+    'subset', 's', 1, 'character', 'Either: Homogenate or Neuron.',
+    'cores', 't', 1, 'integer', 'Number of cores to use',
+#    'chromosome', 'c', 1, 'character', 'One of chr1 to chr22, chrX, chrY or chrM'
+    'permutations', 'p', 1, 'integer', 'Number of permutations to run',
+	'help' , 'h', 0, 'logical', 'Display help'
+), byrow=TRUE, ncol=5)
+opt <- getopt(spec)
+
+## if help was asked for print a friendly message
+## and exit with a non-zero error code
+if (!is.null(opt$help)) {
+	cat(getopt(spec, usage=TRUE))
+	q(status=1)
+}
+
+## For testing
+if(FALSE) {
+    opt <- list('model' = 'cell', 'subset' = 'Neuron', cores = 1,
+        permutations = 0)
+}
+
+## Check inputs
+stopifnot(opt$model %in% c('cell', 'age', 'interaction'))
+stopifnot(opt$subset %in% c('all', 'Homogenate', 'Neuron'))
+if(opt$subset == 'all') stop("Subset = 'all' is not supported.")
+
+
+if(!file.exists(paste0('BSobj_', opt$subset, '.Rdata'))) {
+    ## Load data
+    load("/dcl01/lieber/WGBS/LIBD_Data/bsseqObj/bsseqObj_postNatal_cleaned_CpGonly.rda")
+
+    ## extract pheno
+    pd <- pData(BSobj)
+
+    ## Keep only a given cell type if specified
+    if(opt$subset != 'all') {
+        BSobj <- BSobj[, which(pd$Cell.Type %in% c(opt$subset, 'Glia'))]
+        pd <- pData(BSobj)
+    }
+
+    ## Filter low coverage bases
+    cov <- getCoverage(BSobj, type = 'Cov')
+    cov.ge1 <- cov >= 1
+    cov.filt <- rowSums(cov.ge1) == ncol(cov)
+    print("Number of bases filtered")
+    table(cov.filt)
+#    FALSE     TRUE
+#  3655383 24562065
+    BSobj <- BSobj[cov.filt, ]
+    rm(cov, cov.ge1, cov.filt)
+    
+    save(BSobj, file = paste0('BSobj_', opt$subset, '.Rdata'))
+    
+    ## Get the top million most variable CpGs
+    rvar <- genefilter::rowVars(getMeth(BSobj, type = 'raw'))
+    rvar.filt <- which(order(rvar, decreasing = TRUE) %in% seq_len(1e6))
+    BSobj_top <- BSobj[rvar.filt, ]
+    save(BSobj_top, file = paste0('BSobj_', opt$subset, '_topMillionVar.Rdata'))
+    rm(rvar, rvar.filt)
+    
+} else {
+    ## Smooth with bsseq
+    if(!file.exists(paste0('BSobj_bsseqSmooth_', opt$subset, '.Rdata'))) {
+        load(paste0('BSobj_', opt$subset, '.Rdata'))
+        BSobj <- BSmooth(BSobj, mc.cores = opt$cores, parallelBy = 'sample')
+        save(BSobj, file = paste0('BSobj_bsseqSmooth', opt$subset, '.Rdata'))
+    } else {
+        load(paste0('BSobj_bsseqSmooth_', opt$subset, '.Rdata'))
+    }
+    pd <- pData(BSobj)
+}
+
+
+print(paste('Number of samples used:', nrow(pd)))
+
+coef <- 2
+if(opt$model == 'cell') {
+    design <- with(pd, model.matrix(~ Cell.Type + Age))
+    cut <- 0.1
+} else if (opt$model == 'age') {
+    design <- with(pd, model.matrix(~ Age + Cell.Type))
+    cut <- 0.005
+} else if (opt$model == 'interaction') {
+    design <- with(pd, model.matrix(~ Age * Cell.Type))
+    coef <- grep(':', colnames(design))
+    cut <- 0.009
+}
+
+## Get chr coordinates and methylation values
+gr <- granges(BSobj)
+#gr <- gr[1:1e6]
+## This codes leads to NaN values when not filtering
+
+meth <- getMeth(BSobj, type = 'smooth')
+#meth <- getMeth(BSobj[1:1e6,], type = 'raw')
+## Use this code when not filtering
+#meth <- getCoverage(BSobj, type = 'M')  / (getCoverage(BSobj, type = 'Cov') + 1e-05)
+stopifnot(all(is.finite(range(meth))))
+
+
+## Free some memory
+rm(BSobj)
+
+## Define parallel environment
+registerDoParallel(cores = opt$cores)
+
+## Run bumphunter
+bumps <- bumphunter(object = meth,
+    design = design, chr = as.character(seqnames(gr)), pos = start(gr),
+    maxGap = 1000, B = opt$permutations, coef = coef, cutoff = cut,
+    nullMethod = 'bootstrap', smooth = FALSE)
+
+## Save results
+save(bumps, pd, file = paste0('bumps_bsseqSmooth_', opt$subset, '_', opt$model,
+    '_', opt$permutations, '.Rdata'))
+
+## Reproducibility info
+proc.time()
+message(Sys.time())
+options(width = 120)
+session_info()
