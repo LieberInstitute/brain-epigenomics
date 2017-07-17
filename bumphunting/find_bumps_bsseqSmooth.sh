d4b7ccd6765fd69ef48e92708c7337231a964c4b
Leonardo Collado Torres <lcollado@jhsph.edu>
2017-07-17 19:14:02 -0400

    Now that the smoothed object has been created, run the remaining 2 models (with less mem)

--- bumphunting/find_bumps_bsseqSmooth.sh
+++ bumphunting/find_bumps_bsseqSmooth.sh
@@ -21,7 +21,7 @@ echo "Creating script for chromosome Glia + ${cell} using model ${model} with ${
 cat > .${SHORT}.sh <<EOF
 #!/bin/bash
 #$ -cwd
-#$ -l bluejay,mem_free=60G,h_vmem=60G,h_fsize=100G
+#$ -l bluejay,mem_free=45G,h_vmem=45G,h_fsize=100G
 #$ -N ${SHORT}
 #$ -pe local ${CORES}
 #$ -o ./logs/${SHORT}.txt
