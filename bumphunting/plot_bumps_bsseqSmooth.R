f5d6a9dc1b35f04a625e91cbb2e964c080d4fe1b
Leonardo Collado Torres <lcollado@jhsph.edu>
2017-07-18 09:37:32 -0400

    Add code for plotting top bumps

---
+++ bumphunting/plot_bumps_bsseqSmooth.R
@@ -0,0 +1,66 @@
+library('bsseq')
+library('bumphunter')
+library('devtools')
+library('getopt')
+
+## Specify parameters
+spec <- matrix(c(
+	'model', 'm', 1, 'character', 'Either: cell, age or interaction',
+    'subset', 's', 1, 'character', 'Either: Homogenate or Neuron.',
+    'cores', 't', 1, 'integer', 'Number of cores to use',
+#    'chromosome', 'c', 1, 'character', 'One of chr1 to chr22, chrX, chrY or chrM'
+    'permutations', 'p', 1, 'integer', 'Number of permutations to run',
+	'help' , 'h', 0, 'logical', 'Display help'
+), byrow=TRUE, ncol=5)
+opt <- getopt(spec)
+
+## if help was asked for print a friendly message
+## and exit with a non-zero error code
+if (!is.null(opt$help)) {
+	cat(getopt(spec, usage=TRUE))
+	q(status=1)
+}
+
+## For testing
+if(FALSE) {
+    opt <- list('model' = 'cell', 'subset' = 'Neuron',
+        permutations = 0)
+}
+
+## Check inputs
+inputFile <- paste0('bumps_bsseqSmooth_', opt$subset, '_', opt$model,
+    '_', opt$permutations, '.Rdata')
+stopifnot(file.exists(inputFile))
+
+load(inputFile)
+
+load("../processed_beta_values_plusMap.rda")
+
+## tale of top loci
+tab = bumps$table[1:100,]
+## top CpGs
+topInds = mapply(function(s,e) s:e, tab$indexStart, tab$indexEnd)
+
+meanMeth = sapply(topInds, function(ii) colMeans(t(t(meth[ii,]))))
+meanMeth = do.call("rbind", meanMeth)
+
+pdfFile <- paste0('bumps_bsseqSmooth_', opt$subset, '_', opt$model,
+    '_', opt$permutations, '.pdf')
+
+pdf(pdfFile)
+for(i in 1:nrow(meanMeth)) {
+	plot(meanMeth[i,] ~ pd$Age, pch = 21,
+		bg = factor(pd$Cell.Type),ylim = c(0,1),
+		ylab = "DNAm Level")
+	abline(lm(meanMeth[i,] ~ pd$Age, subset = pd$Cell.Type =="Glia"),
+		col = 1)
+	abline(lm(meanMeth[i,] ~ pd$Age, subset = pd$Cell.Type =="Neuron"),
+		col = 2)
+}
+dev.off()
+
+## Reproducibility info
+proc.time()
+message(Sys.time())
+options(width = 120)
+session_info()
---
+++ bumphunting/plot_bumps_bsseqSmooth.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+## Usage:
+# sh plot_bumps_bsseqSmooth.sh
+
+mkdir -p logs
+
+for cell in Neuron
+do
+    for model in cell age interaction
+    do
+        for permutations in 0
+        do
+
+SHORT="plotting_bumps_bsseqSmooth_${cell}_${model}_${permutations}"
+
+# Construct shell file
+echo "Creating script for chromosome Glia + ${cell} using model ${model} with ${permutations} permutations"
+
+cat > .${SHORT}.sh <<EOF
+#!/bin/bash
+#$ -cwd
+#$ -l bluejay,mem_free=45G,h_vmem=45G,h_fsize=100G
+#$ -N ${SHORT}
+#$ -o ./logs/${SHORT}.txt
+#$ -e ./logs/${SHORT}.txt
+#$ -m e
+
+echo "**** Job starts ****"
+date
+
+Rscript plot_bumps_bsseqSmooth.R -m ${model} -s ${cell} -p ${permutations}
+
+echo "**** Job ends ****"
+date
+EOF
+
+call="qsub .${SHORT}.sh"
+echo $call
+$call
+        done
+    done
+done
