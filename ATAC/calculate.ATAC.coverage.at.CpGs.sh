48827b63d1b83ccd0e5a3d51cdae29ebee4d82a6
Amanda Price <aprice26@compute-080.cm.cluster>
2017-08-03 11:40:54 -0400

    updated to include CpG bed file and changed to pooled sample beds rather than each individual run of each sample

--- ATAC/calculate.ATAC.coverage.at.CpGs.sh
+++ ATAC/calculate.ATAC.coverage.at.CpGs.sh
@@ -3,13 +3,10 @@
 FILELIST=/dcl01/lieber/ajaffe/Amanda/ATAC/FinalPeaks/scripts/finalizedList-pooled.txt
 ID=$(awk "NR==$SGE_TASK_ID" $FILELIST)
 
-PEhs3=/dcl01/lieber/ajaffe/Amanda/ATAC/FinalPeaks/individual_sample_peaks/${ID}.final.10bp-added2.sorted.bed
-SEhs3=/dcl01/lieber/ajaffe/Amanda/ATAC/BAMs/unmerged_remaining_SE/HS3000/${ID}_SEhs3.final.filtered.sorted.bed
-SEhs2=/dcl01/lieber/ajaffe/Amanda/ATAC/BAMs/unmerged_remaining_SE/HS2000/${ID}_SEhs2.final.filtered.sorted.bed
+CPGs=/dcl01/lieber/ajaffe/lab/brain-epigenomics/bumphunting/BSobj_bsseqSmooth_Neuron_minCov_3.bed
+BED=/dcl01/lieber/ajaffe/Amanda/ATAC/BAMs/combined_set/${ID}.final.10bp-added2.bed
 
 cd /dcl01/lieber/ajaffe/Amanda/ATAC/Coverage/coverage_at_CpGs
 module load bedtools/2.24.0
 
-coverageBed -sorted -g ../../genome_files/hg19.genome.reordered -counts -a /dcl01/lieber/ajaffe/Amanda/ATAC/Coverage/coverage_at_CpGs/{{{CPGS_FILE}}}.bed -b $PEhs3 > ${ID}_PEhs3_CpGs_coverageBed.txt
-coverageBed -sorted -g ../../genome_files/hg19.genome.reordered -counts -a /dcl01/lieber/ajaffe/Amanda/ATAC/Coverage/coverage_at_CpGs/{{{CPGS_FILE}}}.bed -b $SEhs3 > ${ID}_SEhs3_CpGs_coverageBed.txt
-coverageBed -sorted -g ../../genome_files/hg19.genome.reordered -counts -a /dcl01/lieber/ajaffe/Amanda/ATAC/Coverage/coverage_at_CpGs/{{{CPGS_FILE}}}.bed -b $SEhs2 > ${ID}_SEhs2_CpGs_coverageBed.txt
+coverageBed -sorted -g ../../genome_files/hg19.genome.reordered -counts -a $CPGs -b $BED > ${ID}_CpGs_coverageBed.txt
