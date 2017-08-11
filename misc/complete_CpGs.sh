16860c4db15cf11f8d95d59811995b24db4a05e8
Leonardo Collado Torres <lcollado@jhsph.edu>
2017-08-11 11:24:47 -0400

    Add script based on Amanda's /dcl01/lieber/ajaffe/lab/brain-epigenomics/ATAC/ATAC_at_CpGs/calculate.ATAC.coverage.at.CpGs.sh

---
+++ misc/complete_CpGs.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#$ -cwd
+#$ -N complete_CpGs
+#$ -o ./complete_CpGs.$TASK_ID.txt
+#$ -e ./complete_CpGs.$TASK_ID.txt
+#$ -m e
+#$ -l bluejay,mem_free=20G,h_vmem=20G,h_fsize=100G
+#$ -t 2,15
+echo "**** Job starts ****"
+date
+
+echo "**** JHPCE info ****"
+echo "User: ${USER}"
+echo "Job id: ${JOB_ID}"
+echo "Job name: ${JOB_NAME}"
+echo "Hostname: ${HOSTNAME}"
+echo "Task id: ${TASK_ID}"
+
+FILELIST=/dcl01/lieber/ajaffe/Amanda/ATAC/FinalPeaks/scripts/finalizedList-pooled.txt
+ID=$(awk "NR==$SGE_TASK_ID" $FILELIST)
+
+CPGs=/dcl01/lieber/ajaffe/Amanda/ATAC/Coverage/coverage_at_CpGs/BSobj_bsseqSmooth_Neuron_minCov_3_resized_sorted.bed
+sortedbed=/dcl01/lieber/ajaffe/Amanda/ATAC/BAMs/combined_set/${ID}.final.sorted.bed
+
+module load bedtools/2.24.0
+
+echo ${ID}
+
+coverageBed -sorted -g ../../genome_files/hg19.genome.reordered2 -counts -a $CPGs -b $sortedbed > /dcl01/lieber/ajaffe/Amanda/ATAC/Coverage/coverage_at_CpGs/${ID}_CpGs_coverageBed_versionByLeo.txt
+
+echo "**** Job ends ****"
+date
