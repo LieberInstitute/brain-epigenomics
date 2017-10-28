948d0e224c6fe964c4c7706967d54a101c9ccee4
lcolladotor <lcollado@jhsph.edu>
2017-10-27 23:20:44 -0400

    Update ATAC fullCov code now that it finally ran successfully

--- misc/atacFullCov.R
+++ misc/atacFullCov.R
@@ -1,4 +1,5 @@
 library('derfinder')
+library('devtools')
 
 bams <- dir('/dcl01/lieber/ajaffe/Amanda/ATAC/BAMs/combined_set/', pattern = '.final.bam$')
 
@@ -10,7 +11,7 @@ stopifnot(length(unique(samples)) == length(bams))
 stopifnot(all(file.exists(bams)))
 stopifnot(all(file.exists(paste0(bams, '.bai'))))
 
-atacFullCov <- fullCoverage(files = bams, chrs = paste0('chr', c(1:22, 'X', 'Y', 'M')), mc.cores = 10)
+atacFullCov <- fullCoverage(files = bams, chrs = paste0('chr', c(1:22, 'X', 'Y', 'M')), mc.cores = 5)
 
 dir.create('rdas', showWarnings = FALSE)
 save(atacFullCov, file = 'rdas/atacFullCov.Rdata')
