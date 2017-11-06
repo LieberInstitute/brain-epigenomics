df83cd808d51931edabb8e95e8ebff28b4fa2056
aprice26 <amanda.joy.price@gmail.com>
2017-11-06 09:55:55 -0500

    calculate number of nonCGs that are changed by age

--- non-CpG/code/nonCG_byCellType_analysis.R
+++ non-CpG/code/nonCG_byCellType_analysis.R
@@ -10,3 +10,13 @@ table(CH[which(CH$padj.CellType<=0.05),"CT.dir"]=="pos")
 #  FALSE    TRUE 
 #  79239 7602836
 # 99% of all significantly regulated sites by cell type are more methylated in neurons
+
+table(CH$Age.dir=="pos")
+#FALSE     TRUE 
+#9843075 30975667
+#75.9% are increasing over age
+
+table(CH[which(CH$padj.Age<=0.05),"CT.dir"]=="pos")
+#FALSE    TRUE 
+#24967 3169651 
+# 99.2% are increasing over age in significantly dmCH sites.
