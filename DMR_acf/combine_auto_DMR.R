b4508714ee24decb9cfd82af37b6bdbd54a302a0
Leonardo Collado Torres <lcollado@jhsph.edu>
2018-01-19 21:02:53 -0500

    Update combine_auto

--- DMR_acf/combine_auto_DMR.R
+++ DMR_acf/combine_auto_DMR.R
@@ -6,10 +6,8 @@ library('ggplot2')
 files <- dir('rda', pattern = '^auto_long', full.names = TRUE)
 
 load_auto <- function(f) {
-    chr <- gsub('_.*', '', gsub('auto_long_', '', f))
     message(paste(Sys.time(), 'loading', f))
     load(f)
-    auto_long$chr <- chr
     return(auto_long)
 }
 
@@ -22,21 +20,24 @@ if(!file.exists('auto_long_combined.Rdata')) {
 dim(auto_long)
 
 ## This never finished running:
-# context_summary <- lapply(unique(auto_long)$context, function(context) {
-#     sub <- auto_long[auto_long$context == context, ]
-#     tapply(sub$acf, sub$lag, summary)
+# mod_context_summary <- lapply(unique(auto_long)$model, function(model) {
+#     lapply(unique(auto_long)$context, function(context) {
+#          sub <- auto_long[auto_long$context == context & auto_long$model == model, ]
+#          tapply(sub$acf, sub$lag, summary)
+#      })
 # })
-# context_summary
-# save(context_summary, file = 'autocorrelation_summary_by_context.Rdata')
+# mod_context_summary
+# save(mod_context_summary, file = 'autocorrelation_summary_by_context_and_model.Rdata')
 
 auto_long$lag <- as.factor(auto_long$lag)
 
-png('autocorrelation_by_context.png', width = 480 * 2)
-ggplot(auto_long, aes(x = lag, y = acf)) + geom_boxplot() + facet_grid(. ~ context) + theme_bw(base_size = 14)
+dir.create('pdf', showWarnings = FALSE)
+png('pdf/autocorrelation_by_context_and_model.png', width = 480 * 2, height = 480 * 3, type = 'cairo')
+ggplot(auto_long, aes(x = lag, y = acf)) + geom_boxplot() + facet_grid(model ~ context) + theme_bw(base_size = 14)
 dev.off()
 
-png('autocorrelation_by_context_abs.png', width = 480 * 2)
-ggplot(auto_long, aes(x = lag, y = abs(acf))) + geom_boxplot() + facet_grid(. ~ context) + theme_bw(base_size = 14)
+png('pdf/autocorrelation_by_context_and_model_abs.png', width = 480 * 2, height = 480 * 3, type = 'cairo')
+ggplot(auto_long, aes(x = lag, y = abs(acf))) + geom_boxplot() + facet_grid(model ~ context) + theme_bw(base_size = 14)
 dev.off()
 
 ## Reproducibility information
