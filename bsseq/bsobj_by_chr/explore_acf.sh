8a73f7695bf275f130d03cdf42ed0006f2d9193f
Leonardo Collado Torres <lcollado@jhsph.edu>
2018-01-24 18:02:38 -0500

    Use bluejay instead of shared, run for all chrs

--- bsseq/bsobj_by_chr/explore_acf.sh
+++ bsseq/bsobj_by_chr/explore_acf.sh
@@ -9,7 +9,7 @@ mkdir -p logs
 #for chrnum in M Y 21 22
 #for chrnum in {2..20} X
 #for chrnum in {2..22} X Y M
-for chrnum in 8 10 11 X
+for chrnum in {1..22} X Y M
 do 
 
     chr="chr${chrnum}"
@@ -25,7 +25,7 @@ do
     cat > .${SHORT}.sh <<EOF
 #!/bin/bash
 #$ -cwd
-#$ -l mem_free=75G,h_vmem=80G,h_fsize=100G
+#$ -l bluejay,mem_free=75G,h_vmem=80G,h_fsize=100G
 #$ -N ${SHORT}
 #$ -o ./logs/${SHORT}.txt
 #$ -e ./logs/${SHORT}.txt
@@ -34,6 +34,7 @@ do
 echo "**** Job starts ****"
 date
 
+module load conda_R/3.4.x
 Rscript explore_acf.R -c ${chr} -t 1 -x ${context}
 
 echo "**** Job ends ****"
