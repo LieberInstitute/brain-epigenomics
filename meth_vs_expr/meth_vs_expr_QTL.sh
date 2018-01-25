234e23ec03ae1fcd7095e3a69a7df444b9865b9b
lcolladotor <lcollado@jhsph.edu>
2018-01-25 11:52:30 -0500

    tweak meth vs expr code

--- meth_vs_expr/meth_vs_expr_QTL.sh
+++ meth_vs_expr/meth_vs_expr_QTL.sh
@@ -6,7 +6,7 @@
 
 mkdir -p logs
 
-for cpg in TRUE FALSE
+for cpg in TRUE #FALSE
 do
     for type in gene exon jx psi
     do
@@ -19,7 +19,7 @@ echo "Creating script for cpg ${cpg} for feature type ${type}"
 cat > .${SHORT}.sh <<EOF
 #!/bin/bash
 #$ -cwd
-#$ -l bluejay,mem_free=170G,h_vmem=170G,h_fsize=200G
+#$ -l mem_free=170G,h_vmem=170G,h_fsize=200G
 #$ -N ${SHORT}
 #$ -o ./logs/${SHORT}.txt
 #$ -e ./logs/${SHORT}.txt
