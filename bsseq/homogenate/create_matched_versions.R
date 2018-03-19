55dbc927ad20d3a028d1f7dc7cb514ff652962ec
Leonardo Collado Torres <lcollado@jhsph.edu>
2018-03-19 10:14:02 -0400

    Try filtering before, to see if it reduces memory load

--- bsseq/homogenate/create_matched_versions.R
+++ bsseq/homogenate/create_matched_versions.R
@@ -5,16 +5,27 @@ library('devtools')
 message(paste(Sys.time(), 'Combining results for the chrs'))
 files <- dir('rda', pattern = '_cleaned_CX_Homogenate.Rdata', full.names = TRUE)
 
+load('../lister/gr_all_highCov.Rdata', verbose = TRUE)
+load('../lister/gr_cps_minCov_3.Rdata', verbose = TRUE)
+gr_cpgs$c_context <- Rle('CG')
+gr_cpgs$trinucleotide_context <- Rle(NA)
+gr_all <- c(gr_all_highCov, gr_cpgs)
+
 ## Load and filter:
 load_filt <- function(f) {
     message(paste(Sys.time(), 'loading', f))
     load(f)
     ## Tweak reportFiles so they can be combined
     colData(BSobj)$reportFiles <- gsub('chr.*', '', colData(BSobj)$reportFiles)
+    
+    message(paste(Sys.time(), 'filtering to our bases'))
+    BSobj <- BSobj[subjectHits(findOverlaps(gr_all, rowRanges(BSobj))), ]
+    
     return(BSobj)
 }
 
 BSobj <- do.call(rbind, lapply(files, load_filt))
+rm(gr_all)
 
 ## Save final combined result
 message(paste(Sys.time(), 'saving allChrs_cleaned_CX_Homogenate.Rdata'))
@@ -23,7 +34,7 @@ save(BSobj, file = 'allChrs_cleaned_CX_Homogenate.Rdata')
 
 ## nonCG next
 message(paste(Sys.time(), 'filtering nonCpGs'))
-load('../lister/gr_all_highCov.Rdata', verbose = TRUE)
+
 gr <- rowRanges(BSobj)
 
 Cov <- M <- matrix(0, nrow = length(gr_all_highCov), ncol = ncol(BSobj))
@@ -41,7 +52,6 @@ rm(BSobj, gr, Cov, M, ov, gr_all_highCov)
 
 ## CG next
 message(paste(Sys.time(), 'filtering CpGs'))
-load('../lister/gr_cps_minCov_3.Rdata', verbose = TRUE)
 load('allChrs_cleaned_CX_Homogenate.Rdata', verbose = TRUE)
 Cov <- M <- matrix(0, nrow = length(gr_cpgs), ncol = ncol(BSobj))
 colnames(Cov) <- colnames(M) <- colnames(BSobj)
