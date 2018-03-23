2ea5e4d1fca8a580c92dcfd52ca8475d0ba46be4
lcolladotor <lcollado@jhsph.edu>
2018-03-23 14:51:29 -0400

    Try the more step by step approach I described in Slack. It should (in theory) require less mem, we'll see

--- bsseq/homogenate/create_matched_versions.R
+++ bsseq/homogenate/create_matched_versions.R
@@ -1,78 +1,129 @@
 library('bsseq')
 library('GenomicRanges')
 library('devtools')
+library('getopt')
+
+## Specify parameters
+spec <- matrix(c(
+    'chr', 'c', 1, 'character', 'chromosome to subset',
+    'type', 't', 2, 'character', 'methylation type: either CpG or CpH',
+	'help' , 'h', 0, 'logical', 'Display help'
+), byrow=TRUE, ncol=5)
+opt <- getopt(spec)
+
+
+## if help was asked for print a friendly message
+## and exit with a non-zero error code
+if (!is.null(opt$help)) {
+	cat(getopt(spec, usage=TRUE))
+	q(status=1)
+}
+
+## For testing
+if(FALSE) {
+    opt <- list(chr = 'chr21', type = 'CpG')
+}
+
+
+if(opt$chr != 'all') {
+    load('../lister/gr_all_highCov.Rdata', verbose = TRUE)
+    load('../lister/gr_cps_minCov_3.Rdata', verbose = TRUE)
+    gr_cpgs$c_context <- Rle('CG')
+    gr_cpgs$trinucleotide_context <- Rle(NA)
+    gr_all <- c(gr_all_highCov, gr_cpgs)
+    
+    f <- paste0('rda/', opt$chr, '_cleaned_CX_Homogenate.Rdata')
+    message(paste(Sys.time(), 'loading', f))
+    load(f, verbose = TRUE)
+    
+    ## Tweak reportFiles so they can be combined
+    colData(BSobj)$reportFiles <- gsub('chr.*', '', colData(BSobj)$reportFiles)
+    BSobj_all <- BSobj
+    
+    message(paste(Sys.time(), 'filtering to our CpG bases'))
+    BSobj <- BSobj_all[subjectHits(findOverlaps(gr_cpgs, rowRanges(BSobj_all))), ]
+    
+    message(paste(Sys.time(), 'saving filtered version'))
+    save(BSobj, file = paste0('rda/', opt$chr, '_cleaned_CX_Homogenate_filtered_CpG.Rdata'))
+    
+    message(paste(Sys.time(), 'filtering to our CpH bases'))
+    BSobj <- BSobj_all[subjectHits(findOverlaps(gr_all_highCov, rowRanges(BSobj_all))), ]
+    
+    message(paste(Sys.time(), 'saving filtered version'))
+    save(BSobj, file = paste0('rda/', opt$chr, '_cleaned_CX_Homogenate_filtered_CpH.Rdata'))
+    
+    ## Reproducibility information
+    print('Reproducibility information:')
+    Sys.time()
+    proc.time()
+    options(width = 120)
+    session_info()
+    
+    q('no')
+}
+
+stopifnot(opt$type %in% c('CpG', 'CpH'))
+
 
 message(paste(Sys.time(), 'Combining results for the chrs'))
-files <- dir('rda', pattern = '_cleaned_CX_Homogenate.Rdata', full.names = TRUE)
+patt <- paste('_cleaned_CX_Homogenate_filtered_', ifelse(opt$type == 'CpG', 'CpG', 'CpH'), '.Rdata')
 
-load('../lister/gr_all_highCov.Rdata', verbose = TRUE)
-load('../lister/gr_cps_minCov_3.Rdata', verbose = TRUE)
-gr_cpgs$c_context <- Rle('CG')
-gr_cpgs$trinucleotide_context <- Rle(NA)
-gr_all <- c(gr_all_highCov, gr_cpgs)
+files <- dir('rda', pattern = patt, full.names = TRUE)
+
+
+
 
 ## Load and filter:
 load_filt <- function(f) {
-    f_filtered <- gsub('.Rdata', '_filtered.Rdata', f)
-    if(file.exists(f_filtered)) {
-        message(paste(Sys.time(), 'loading', f_filtered))
-        load(f, verbose = TRUE)
-        return(BSobj)
-    }
-    
     message(paste(Sys.time(), 'loading', f))
-    load(f, verbose = TRUE)
-    ## Tweak reportFiles so they can be combined
-    colData(BSobj)$reportFiles <- gsub('chr.*', '', colData(BSobj)$reportFiles)
-    
-    message(paste(Sys.time(), 'filtering to our bases'))
-    BSobj <- BSobj[subjectHits(findOverlaps(gr_all, rowRanges(BSobj))), ]
-    
-    message(paste(Sys.time(), 'saving filtered version'))
-    save(BSobj, file = f_filtered)
-    
+    load(f, verbose = TRUE)    
     return(BSobj)
 }
 
-BSobj <- do.call(rbind, lapply(files, load_filt))
-rm(gr_all)
+f_filt <- paste0('allChrs_cleaned_CX_Homogenate_', opt$type, '.Rdata')
+if(!file.exists(f_filt)) {
+    BSobj <- do.call(rbind, lapply(files, load_filt))
 
-## Save final combined result
-message(paste(Sys.time(), 'saving allChrs_cleaned_CX_Homogenate.Rdata'))
-save(BSobj, file = 'allChrs_cleaned_CX_Homogenate.Rdata')
+    ## Save final combined result
+    message(paste(Sys.time(), 'saving', f_filt))
+    save(BSobj, file = f_filt)
+} else {
+    message(paste(Sys.time(), 'loading', f_filt))
+    load(f_filt, verbose = TRUE)
+}
 
 
-## nonCG next
-message(paste(Sys.time(), 'filtering nonCpGs'))
+if(opt$type == 'CpG') {
+    ## nonCG next
+    message(paste(Sys.time(), 'filtering nonCpGs'))
 
-gr <- rowRanges(BSobj)
+    gr <- rowRanges(BSobj)
 
-Cov <- M <- matrix(0, nrow = length(gr_all_highCov), ncol = ncol(BSobj))
-colnames(Cov) <- colnames(M) <- colnames(BSobj)
+    Cov <- M <- matrix(0, nrow = length(gr_all_highCov), ncol = ncol(BSobj))
+    colnames(Cov) <- colnames(M) <- colnames(BSobj)
 
-ov <- findOverlaps(gr_all_highCov, rowRanges(BSobj))
-M[queryHits(ov), ] <- assays(BSobj)$M[subjectHits(ov), ]
-Cov[queryHits(ov), ] <- assays(BSobj)$Cov[subjectHits(ov), ]
+    ov <- findOverlaps(gr_all_highCov, rowRanges(BSobj))
+    M[queryHits(ov), ] <- assays(BSobj)$M[subjectHits(ov), ]
+    Cov[queryHits(ov), ] <- assays(BSobj)$Cov[subjectHits(ov), ]
 
-BSobj <- BSseq(M = M, Cov = Cov, gr = gr_all_highCov, pData = colData(BSobj))
-message(paste(Sys.time(), 'saving allChrs_matched_cleaned_CX_Homogenate_nonCG_highCov.Rdata'))
-save(BSobj, file = 'allChrs_matched_cleaned_CX_Homogenate_nonCG_highCov.Rdata')
+    BSobj <- BSseq(M = M, Cov = Cov, gr = gr_all_highCov, pData = colData(BSobj))
+    message(paste(Sys.time(), 'saving allChrs_matched_cleaned_CX_Homogenate_nonCG_highCov.Rdata'))
+    save(BSobj, file = 'allChrs_matched_cleaned_CX_Homogenate_nonCG_highCov.Rdata')
+} else {
+    ## CG next
+    message(paste(Sys.time(), 'filtering CpGs'))
+    load('allChrs_cleaned_CX_Homogenate.Rdata', verbose = TRUE)
+    Cov <- M <- matrix(0, nrow = length(gr_cpgs), ncol = ncol(BSobj))
+    colnames(Cov) <- colnames(M) <- colnames(BSobj)
 
-rm(BSobj, gr, Cov, M, ov, gr_all_highCov)
+    ov <- findOverlaps(gr_cpgs, rowRanges(BSobj))
+    M[queryHits(ov), ] <- assays(BSobj)$M[subjectHits(ov), ]
+    Cov[queryHits(ov), ] <- assays(BSobj)$Cov[subjectHits(ov), ]
 
-## CG next
-message(paste(Sys.time(), 'filtering CpGs'))
-load('allChrs_cleaned_CX_Homogenate.Rdata', verbose = TRUE)
-Cov <- M <- matrix(0, nrow = length(gr_cpgs), ncol = ncol(BSobj))
-colnames(Cov) <- colnames(M) <- colnames(BSobj)
-
-ov <- findOverlaps(gr_cpgs, rowRanges(BSobj))
-M[queryHits(ov), ] <- assays(BSobj)$M[subjectHits(ov), ]
-Cov[queryHits(ov), ] <- assays(BSobj)$Cov[subjectHits(ov), ]
-
-BSobj <- BSseq(M = M, Cov = Cov, gr = gr_cpgs, pData = colData(BSobj))
-message(paste(Sys.time(), 'saving BSobj_matched_cleaned_CX_Homogenate_minCov_3.Rdata'))
-save(BSobj, file = 'BSobj_matched_cleaned_CX_Homogenate_minCov_3.Rdata')
+    BSobj <- BSseq(M = M, Cov = Cov, gr = gr_cpgs, pData = colData(BSobj))
+    message(paste(Sys.time(), 'saving BSobj_matched_cleaned_CX_Homogenate_minCov_3.Rdata'))
+    save(BSobj, file = 'BSobj_matched_cleaned_CX_Homogenate_minCov_3.Rdata')
+}
 
 ## Reproducibility information
 print('Reproducibility information:')
