e4a94be2dbb57208b0a227f26803f9c1a95cfdc2
lcolladotor <lcollado@jhsph.edu>
2018-03-27 15:36:47 -0400

    Move the coercing code to the per chr section of the code

--- bsseq/homogenate/create_matched_versions.sh
+++ bsseq/homogenate/create_matched_versions.sh
@@ -52,7 +52,7 @@ echo "Creating script for type ${type}"
 cat > .${SHORT}.sh <<EOF
 #!/bin/bash
 #$ -cwd
-#$ -l mem_free=400G,h_vmem=400G,h_fsize=300G
+#$ -l mem_free=300G,h_vmem=300G,h_fsize=300G
 #$ -N ${SHORT}
 #$ -o ./logs/${SHORT}.txt
 #$ -e ./logs/${SHORT}.txt
