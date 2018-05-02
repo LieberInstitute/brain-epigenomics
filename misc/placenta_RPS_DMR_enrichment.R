41c7451e0d36431b0099b58acb4d777a43f65aaf
aprice26 <amanda.joy.price@gmail.com>
2018-05-02 15:53:17 -0400

    compare placenta and non-placenta RPS enrichment for DMRs

---
+++ misc/placenta_RPS_DMR_enrichment.R
@@ -0,0 +1,167 @@
+library(GenomicRanges)
+library(bumphunter)
+library(SNPlocs.Hsapiens.dbSNP144.GRCh37)
+
+load('/dcl01/lieber/ajaffe/lab/brain-epigenomics/bumphunting/BSobj_bsseqSmooth_Neuron_minCov_3.Rdata')
+load("/dcl01/lieber/ajaffe/lab/brain-epigenomics/rdas/DMR/DMR_objects.rda")
+load("/dcl01/lieber/ajaffe/lab/brain-epigenomics/bumphunting/rda/limma_Neuron_CpGs_minCov_3_ageInfo_dmrs.Rdata")
+
+
+# Load SNPs
+
+plac = scan("/dcl01/lieber/ajaffe/lab/brain-epigenomics/misc/rps.snpZ4D.p5e-08.snp", what = "character")
+nonplac = scan("/dcl01/lieber/ajaffe/lab/brain-epigenomics/misc/rps.snpZ4Dexc.p5e-08.snp", what = "character")
+
+plac = snpsById(SNPlocs.Hsapiens.dbSNP144.GRCh37, plac)
+nonplac = snpsById(SNPlocs.Hsapiens.dbSNP144.GRCh37, nonplac)
+plac = as.data.frame(plac)
+plac$seqnames = paste0("chr", plac$seqnames)
+plac = makeGRangesFromDataFrame(plac, start.field = "pos", end.field = "pos", keep.extra.columns = T)
+nonplac = as.data.frame(nonplac)
+nonplac$seqnames = paste0("chr", nonplac$seqnames)
+nonplac = makeGRangesFromDataFrame(nonplac, start.field = "pos", end.field = "pos", keep.extra.columns = T)
+
+
+### test LD boundaries, +/- 500kb, and +/- 1Mb
+
+plac500 = as.data.frame(plac)
+plac500$start = plac500$start-500000
+plac500$end = plac500$end+500000
+plac500 = makeGRangesFromDataFrame(plac500, keep.extra.columns = T)
+plac1000 = as.data.frame(plac)
+plac1000$start = plac1000$start-1000000
+plac1000$end = plac1000$end+1000000
+plac1000 = makeGRangesFromDataFrame(plac1000, keep.extra.columns = T)
+
+nonplac500 = as.data.frame(nonplac)
+nonplac500$start = nonplac500$start-500000
+nonplac500$end = nonplac500$end+500000
+nonplac500 = makeGRangesFromDataFrame(nonplac500, keep.extra.columns = T)
+nonplac1000 = as.data.frame(nonplac)
+nonplac1000$start = nonplac1000$start-1000000
+nonplac1000$end = nonplac1000$end+1000000
+nonplac1000 = makeGRangesFromDataFrame(nonplac1000, keep.extra.columns = T)
+
+
+## DMR granges
+
+DMRgr = lapply(DMR, function(x) makeGRangesFromDataFrame(x[which(x$sig=="FWER < 0.05"),], keep.extra.columns = T))
+
+dmrs = split(dmrs, dmrs$k6cluster_label)
+names(dmrs) = c("Gr1","Gr2","Gr3","Gr4","Gr5","Gr6")
+oo = lapply(dmrs, function(x) findOverlaps(x, makeGRangesFromDataFrame(DMR$Interaction)))
+dmrs = lapply(oo, function(x) DMR$Interaction[subjectHits(x),])
+
+DMRgr = c(DMRgr, list("CellType.N+" = DMRgr$CellType[which(DMRgr$CellType$Dir=="pos" & DMRgr$CellType$sig=="FWER < 0.05"),],
+                      "CellType.N-" = DMRgr$CellType[which(DMRgr$CellType$Dir=="neg" & DMRgr$CellType$sig=="FWER < 0.05"),]),
+          lapply(dmrs, function(x) makeGRangesFromDataFrame(x[which(x$sig=="FWER < 0.05"),], keep.extra.columns = T)))
+DMRgr = lapply(DMRgr, reduce)
+
+
+## Test for enrichment of DMRs in regions by CpG clusters
+
+# Identify all CpG clusters in the genome
+gr <- granges(BSobj)
+cl = clusterMaker( chr = as.character(seqnames(gr)), 
+                   pos = start(gr),   maxGap = 1000)
+gr.clusters = split(gr, cl)
+gr.clusters = unlist(range(gr.clusters))
+df.clusters = as.data.frame(gr.clusters)
+
+
+# Find overlaps with DMRS in all three models
+
+oo = lapply(DMRgr, function(x) findOverlaps(gr.clusters, x))
+lapply(oo, function(x) length(unique(queryHits(x))))
+
+df.clusters$regionID = paste0(df.clusters$seqnames,":",df.clusters$start,"-",df.clusters$end)
+df.clusters$rnum = 1:length(gr.clusters)
+df.clusters$CellType = ifelse(df.clusters$rnum %in% queryHits(oo$CellType), "CellType","no")
+df.clusters$"CellType.N+" = ifelse(df.clusters$rnum %in% queryHits(oo$"CellType.N+"), "CellType","no")
+df.clusters$"CellType.N-" = ifelse(df.clusters$rnum %in% queryHits(oo$"CellType.N-"), "CellType","no")
+
+df.clusters$Age = ifelse(df.clusters$rnum %in% queryHits(oo$Age), "Age","no")
+df.clusters$Interaction = ifelse(df.clusters$rnum %in% queryHits(oo$Interaction), "Interaction","no")
+df.clusters$Gr1 = ifelse(df.clusters$rnum %in% queryHits(oo$Gr1), "Gr1","no")
+df.clusters$Gr2 = ifelse(df.clusters$rnum %in% queryHits(oo$Gr2), "Gr2","no")
+df.clusters$Gr3 = ifelse(df.clusters$rnum %in% queryHits(oo$Gr3), "Gr3","no")
+df.clusters$Gr4 = ifelse(df.clusters$rnum %in% queryHits(oo$Gr4), "Gr4","no")
+df.clusters$Gr5 = ifelse(df.clusters$rnum %in% queryHits(oo$Gr5), "Gr5","no")
+df.clusters$Gr6 = ifelse(df.clusters$rnum %in% queryHits(oo$Gr6), "Gr6","no")
+df.clusters$DMRs = paste(df.clusters$CellType, df.clusters$Age, df.clusters$Interaction,
+                         df.clusters$Gr1,df.clusters$Gr2,df.clusters$Gr3,df.clusters$Gr4,df.clusters$Gr5,df.clusters$Gr6, sep=":")
+
+ooP = findOverlaps(plac1000, gr.clusters)
+ooNP = findOverlaps(nonplac1000, gr.clusters)
+df.clusters$plac1000 = ifelse(df.clusters$rnum %in% subjectHits(ooP), "plac1000","no")
+df.clusters$nonplac1000 = ifelse(df.clusters$rnum %in% subjectHits(ooNP), "nonplac1000","no")
+
+ooP = findOverlaps(plac500, gr.clusters)
+ooNP = findOverlaps(nonplac500, gr.clusters)
+df.clusters$plac500 = ifelse(df.clusters$rnum %in% subjectHits(ooP), "plac500","no")
+df.clusters$nonplac500 = ifelse(df.clusters$rnum %in% subjectHits(ooNP), "nonplac500","no")
+
+
+# make contingency tables
+
+cols = c("CellType","CellType.N+","CellType.N-","Age","Interaction","Gr1","Gr2","Gr3","Gr4","Gr5","Gr6")
+tables = list()
+for (i in 1:length(cols)) {
+  tables[[i]] = list(plac500kb = data.frame(YesPGC = c(nrow(df.clusters[df.clusters$plac500=="plac500" & df.clusters[,colnames(df.clusters)==cols[i]]!="no",]),
+                                                       nrow(df.clusters[df.clusters$plac500=="plac500" & df.clusters[,colnames(df.clusters)==cols[i]]=="no",])),
+                                            NoPGC = c(nrow(df.clusters[df.clusters$plac500=="no" & df.clusters[,colnames(df.clusters)==cols[i]]!="no",]),
+                                                      nrow(df.clusters[df.clusters$plac500=="no" & df.clusters[,colnames(df.clusters)==cols[i]]=="no",])), 
+                                            row.names = c("YesDMR","NoDMR")),
+                     nonplac500kb = data.frame(YesPGC = c(nrow(df.clusters[df.clusters$nonplac500=="nonplac500" & df.clusters[,colnames(df.clusters)==cols[i]]!="no",]),
+                                                          nrow(df.clusters[df.clusters$nonplac500=="nonplac500" & df.clusters[,colnames(df.clusters)==cols[i]]=="no",])),
+                                               NoPGC = c(nrow(df.clusters[df.clusters$nonplac500=="no" & df.clusters[,colnames(df.clusters)==cols[i]]!="no",]),
+                                                         nrow(df.clusters[df.clusters$nonplac500=="no" & df.clusters[,colnames(df.clusters)==cols[i]]=="no",])), 
+                                               row.names = c("YesDMR","NoDMR")),
+                     plac1Mb = data.frame(YesPGC = c(nrow(df.clusters[df.clusters$plac1000=="plac1000" & df.clusters[,colnames(df.clusters)==cols[i]]!="no",]),
+                                                     nrow(df.clusters[df.clusters$plac1000=="plac1000" & df.clusters[,colnames(df.clusters)==cols[i]]=="no",])),
+                                          NoPGC = c(nrow(df.clusters[df.clusters$plac1000=="no" & df.clusters[,colnames(df.clusters)==cols[i]]!="no",]),
+                                                    nrow(df.clusters[df.clusters$plac1000=="no" & df.clusters[,colnames(df.clusters)==cols[i]]=="no",])), 
+                                          row.names = c("YesDMR","NoDMR")),
+                     nonplac1Mb = data.frame(YesPGC = c(nrow(df.clusters[df.clusters$nonplac1000=="nonplac1000" & df.clusters[,colnames(df.clusters)==cols[i]]!="no",]),
+                                                        nrow(df.clusters[df.clusters$nonplac1000=="nonplac1000" & df.clusters[,colnames(df.clusters)==cols[i]]=="no",])),
+                                             NoPGC = c(nrow(df.clusters[df.clusters$nonplac1000=="no" & df.clusters[,colnames(df.clusters)==cols[i]]!="no",]),
+                                                       nrow(df.clusters[df.clusters$nonplac1000=="no" & df.clusters[,colnames(df.clusters)==cols[i]]=="no",])), 
+                                             row.names = c("YesDMR","NoDMR")),
+                     placVSnonplac500kb = data.frame(YesPGC = c(nrow(df.clusters[df.clusters$plac500=="plac500" & df.clusters[,colnames(df.clusters)==cols[i]]!="no",]),
+                                                                nrow(df.clusters[df.clusters$plac500=="plac500" & df.clusters[,colnames(df.clusters)==cols[i]]=="no",])),
+                                                     NoPGC = c(nrow(df.clusters[df.clusters$nonplac500=="nonplac500" & df.clusters[,colnames(df.clusters)==cols[i]]!="no",]),
+                                                               nrow(df.clusters[df.clusters$nonplac500=="nonplac500" & df.clusters[,colnames(df.clusters)==cols[i]]=="no",])), 
+                                                     row.names = c("YesDMR","NoDMR")),
+                     placVSnonplac1Mb = data.frame(YesPGC = c(nrow(df.clusters[df.clusters$plac1000=="plac1000" & df.clusters[,colnames(df.clusters)==cols[i]]!="no",]),
+                                                              nrow(df.clusters[df.clusters$plac1000=="plac1000" & df.clusters[,colnames(df.clusters)==cols[i]]=="no",])),
+                                                   NoPGC = c(nrow(df.clusters[df.clusters$nonplac1000=="nonplac1000" & df.clusters[,colnames(df.clusters)==cols[i]]!="no",]),
+                                                             nrow(df.clusters[df.clusters$nonplac1000=="nonplac1000" & df.clusters[,colnames(df.clusters)==cols[i]]=="no",])), 
+                                                   row.names = c("YesDMR","NoDMR")))
+}
+names(tables) = cols
+
+fisher = lapply(tables, function(x) lapply(x,fisher.test))
+df = do.call(rbind, Map(cbind, SigGroup = as.list(names(fisher)), lapply(fisher, function(y) 
+  do.call(rbind, Map(cbind, SNPgroup = as.list(names(y)), lapply(y, function(x) data.frame(pval = x$p.value, OddsRatio = x$estimate)))))))
+df$FDR = p.adjust(df$pval, method = "fdr")
+rownames(df) = NULL
+df$SigGroup = gsub("Gr1", "G-:N+", df$SigGroup)
+df$SigGroup = gsub("Gr2", "G0:N+", df$SigGroup)
+df$SigGroup = gsub("Gr3", "G0:N-", df$SigGroup)
+df$SigGroup = gsub("Gr4", "G+:N0", df$SigGroup)
+df$SigGroup = gsub("Gr5", "G+:N-", df$SigGroup)
+df$SigGroup = gsub("Gr6", "G-:N0", df$SigGroup)
+
+write.csv(df,file="/dcl01/lieber/ajaffe/lab/brain-epigenomics/rdas/DMR/placenta_SNPregions_enrichment_DMRs.csv",quote=F)
+
+df[which(df$FDR<=0.05),]
+
+
+## Associate with LD blocks
+
+pgc = read.delim("/dcl01/lieber/ajaffe/lab/brain-epigenomics/rdas/tableS3_Nature2014_pgc2_loci.txt",as.is=TRUE)
+pgc = GRanges(pgc$Position..hg19.)
+
+ooP = findOverlaps(pgc, plac)
+plac[c(7,19,26,33,35,40,49,50,55,57)]
+
