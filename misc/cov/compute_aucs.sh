bf9171ac5118c50f6a17adcc217b74d8dfd639be
lcolladotor <lcollado@jhsph.edu>
2019-02-06 13:59:40 -0500

    Add code for computing AUCs files with bamcount https://github.com/BenLangmead/bamcount v0.2.6

---
+++ misc/cov/compute_aucs.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+## Usage:
+# sh compute_aucs.sh
+
+mkdir -p logs
+mkdir -p auc_files
+
+CORES=4
+FILELIST=/dcl01/lieber/ajaffe/lab/brain-epigenomics/misc/cov/WGC_IDs_subset.txt
+NUM=$(cat $FILELIST | awk '{print $NF}' | uniq | wc -l)
+
+for bamtype in duplicatesRemoved Marked_duplicates
+do
+    
+    SHORT="compute_aucs_${bamtype}"
+
+    # Construct shell file
+    echo "Creating script for BAM files ${bamtype}"
+    cat > .${SHORT}.sh <<EOF
+#!/bin/bash
+#$ -cwd
+#$ -l mem_free=10G,h_vmem=10G,h_fsize=100G
+#$ -pe local ${CORES}
+#$ -N ${SHORT}
+#$ -o ./logs/${SHORT}.txt
+#$ -e ./logs/${SHORT}.txt
+#$ -m e
+#$ -t 1-${NUM}
+
+echo "**** Job starts ****"
+date
+echo "**** JHPCE info ****"
+echo "User: \${USER}"
+echo "Job id: \${JOB_ID}"
+echo "Job name: \${JOB_NAME}"
+echo "Hostname: \${HOSTNAME}"
+echo "Task id: \${TASK_ID}"
+
+
+ID=\$(awk "NR==\${SGE_TASK_ID}" $FILELIST )
+
+## Load dependencies
+module load bamcount/0.2.6
+
+## List current modules
+module list
+
+## compute aucs
+ls -lh /dcl02/lieber/WGBS/LIBD_Data/BAM/\${ID}*${bamtype}.bam
+bamcount /dcl02/lieber/WGBS/LIBD_Data/BAM/\${ID}*${bamtype}.bam --threads 4 --no_head --auc auc_files/\${ID}_${bamtype}
+
+echo "**** Job ends ****"
+date
+EOF
+    call="qsub .${SHORT}.sh"
+    echo $call
+    $call
+done
