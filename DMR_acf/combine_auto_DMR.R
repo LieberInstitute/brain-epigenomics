f3ab5b9a3164235ee3c7b13256b001052f7e1006
lcolladotor <lcollado@jhsph.edu>
2019-02-11 15:15:33 -0500

    Update images with data sorted by chr position (acf for DMRs and lag by distance)

--- DMR_acf/combine_auto_DMR.R
+++ DMR_acf/combine_auto_DMR.R
@@ -33,20 +33,9 @@ head(auto_long)
 # 6  0.3721603 0.3722272   2    2    638       9007     all   age     28.11321
 
 auto_long[head(which(auto_long$avg_distance < 0)), ]
-#          acf_neuron    acf_glia lag clus clus_n clus_range context model
-# 101732 -0.003217755 -0.06663796   4   10     15       1216     CHG   age
-# 101761 -0.012045398 -0.05505406   1   18     36        945     CHG   age
-# 101762  0.119849050  0.07904448   2   18     36        945     CHG   age
-# 101763 -0.047964536 -0.04692480   3   18     36        945     CHG   age
-# 101764 -0.021825900 -0.02983349   4   18     36        945     CHG   age
-# 101837 -0.068108113 -0.02281280   1   37     13       1145     CHG   age
-#        avg_distance
-# 101732    -4.090909
-# 101761    -2.114286
-# 101762    -5.911765
-# 101763   -19.090909
-# 101764   -34.437500
-# 101837   -12.000000
+# [1] acf_neuron   acf_glia     lag          clus         clus_n
+# [6] clus_range   context      model        avg_distance
+# <0 rows> (or 0-length row.names)
 
 ## This never finished running:
 # mod_context_summary <- lapply(unique(auto_long)$model, function(model) {
@@ -81,20 +70,17 @@ ggplot(sub, aes(x = lag, y = acf_glia)) + geom_boxplot() + facet_grid(~ context)
 dev.off()
 
 
-
+pdf('pdf/bp_distance_by_lag.pdf', width = 10, height = 10, useDingbats = FALSE)
 ggplot(auto_long, aes(x = lag, y = avg_distance)) + geom_boxplot() + facet_grid(model ~ context) + theme_bw(base_size = 30) + ylab('Avg base-pair distance')
-
-
-ggplot(auto_long, aes(x = lag, y = log10(avg_distance + 1))) + geom_boxplot() + facet_grid(model ~ context) + theme_bw(base_size = 30) + ylab('Avg base-pair distance')
 ggplot(auto_long, aes(x = lag, y = avg_distance)) + geom_boxplot() + facet_grid(model ~ context) + theme_bw(base_size = 30) + ylab('Avg base-pair distance') + scale_y_log10()
 
 summary(auto_long$avg_distance)
-#     Min.  1st Qu.   Median     Mean  3rd Qu.     Max.
-# -2236.00    22.42    50.77    94.67   116.08  4538.00
+#  Min.  1st Qu.   Median     Mean  3rd Qu.     Max.
+# 1.333   31.637   64.437  114.359  136.147 4538.000
 
 ggplot(sub, aes(x = lag, y = avg_distance)) + geom_boxplot() + facet_grid(~ context) + theme_bw(base_size = 30) + ylab('Avg base-pair distance')
-
 ggplot(sub, aes(x = lag, y = avg_distance)) + geom_boxplot() + facet_grid(~ context) + theme_bw(base_size = 30) + ylab('Avg base-pair distance')  + scale_y_log10()
+dev.off()
 
 ## Reproducibility information
 print('Reproducibility information:')
@@ -102,54 +88,79 @@ Sys.time()
 proc.time()
 options(width = 120)
 session_info()
-#
-# Session info ----------------------------------------------------------------------------------------------------------
+
+# ─ Session info ───────────────────────────────────────────────────────────────────────────────────────────────────────
 #  setting  value
-#  version  R version 3.4.3 Patched (2018-01-20 r74142)
+#  version  R version 3.5.1 Patched (2018-10-29 r75535)
+#  os       Red Hat Enterprise Linux Server release 6.9 (Santiago)
 #  system   x86_64, linux-gnu
 #  ui       X11
 #  language (EN)
 #  collate  en_US.UTF-8
+#  ctype    en_US.UTF-8
 #  tz       US/Eastern
-#  date     2018-03-13
+#  date     2019-02-11
 #
-# Packages --------------------------------------------------------------------------------------------------------------
-#  package     * version date       source
-#  base        * 3.4.3   2018-01-20 local
-#  colorout    * 1.2-0   2018-02-19 Github (jalvesaq/colorout@2f01173)
-#  colorspace    1.3-2   2016-12-14 CRAN (R 3.4.1)
-#  compiler      3.4.3   2018-01-20 local
-#  datasets    * 3.4.3   2018-01-20 local
-#  devtools    * 1.13.4  2017-11-09 CRAN (R 3.4.2)
-#  digest        0.6.15  2018-01-28 cran (@0.6.15)
-#  ggplot2     * 2.2.1   2016-12-30 CRAN (R 3.4.1)
-#  graphics    * 3.4.3   2018-01-20 local
-#  grDevices   * 3.4.3   2018-01-20 local
-#  grid          3.4.3   2018-01-20 local
-#  gtable        0.2.0   2016-02-26 CRAN (R 3.4.1)
-#  htmltools     0.3.6   2017-04-28 CRAN (R 3.4.1)
-#  htmlwidgets   0.9     2017-07-10 CRAN (R 3.4.1)
-#  httpuv        1.3.6.2 2018-03-02 CRAN (R 3.4.3)
-#  labeling      0.3     2014-08-23 CRAN (R 3.4.1)
-#  lattice       0.20-35 2017-03-25 CRAN (R 3.4.3)
-#  lazyeval      0.2.1   2017-10-29 CRAN (R 3.4.2)
-#  magrittr      1.5     2014-11-22 CRAN (R 3.4.1)
-#  memoise       1.1.0   2017-04-21 CRAN (R 3.4.1)
-#  methods     * 3.4.3   2018-01-20 local
-#  munsell       0.4.3   2016-02-13 CRAN (R 3.4.1)
-#  pillar        1.1.0   2018-01-14 CRAN (R 3.4.2)
-#  plyr          1.8.4   2016-06-08 CRAN (R 3.4.1)
-#  png           0.1-7   2013-12-03 CRAN (R 3.4.1)
-#  Rcpp          0.12.14 2017-11-23 CRAN (R 3.4.2)
-#  reshape2      1.4.3   2017-12-11 CRAN (R 3.4.2)
-#  rlang         0.1.6   2017-12-21 CRAN (R 3.4.2)
-#  rmote       * 0.3.4   2018-02-16 deltarho (R 3.4.3)
-#  scales        0.5.0   2017-08-24 CRAN (R 3.4.1)
-#  servr         0.8     2017-11-06 CRAN (R 3.4.3)
-#  stats       * 3.4.3   2018-01-20 local
-#  stringi       1.1.6   2017-11-17 CRAN (R 3.4.2)
-#  stringr       1.2.0   2017-02-18 CRAN (R 3.4.1)
-#  tibble        1.4.1   2017-12-25 CRAN (R 3.4.2)
-#  tools         3.4.3   2018-01-20 local
-#  utils       * 3.4.3   2018-01-20 local
-#  withr         2.1.1   2017-12-19 CRAN (R 3.4.2)
+# ─ Packages ───────────────────────────────────────────────────────────────────────────────────────────────────────────
+#  package     * version date       lib source
+#  assertthat    0.2.0   2017-04-11 [2] CRAN (R 3.5.0)
+#  backports     1.1.3   2018-12-14 [2] CRAN (R 3.5.1)
+#  bindr         0.1.1   2018-03-13 [1] CRAN (R 3.5.0)
+#  bindrcpp      0.2.2   2018-03-29 [1] CRAN (R 3.5.0)
+#  callr         3.1.1   2018-12-21 [2] CRAN (R 3.5.1)
+#  cli           1.0.1   2018-09-25 [1] CRAN (R 3.5.1)
+#  colorout    * 1.2-0   2018-05-02 [1] Github (jalvesaq/colorout@c42088d)
+#  colorspace    1.4-0   2019-01-13 [2] CRAN (R 3.5.1)
+#  crayon        1.3.4   2017-09-16 [1] CRAN (R 3.5.0)
+#  desc          1.2.0   2018-05-01 [2] CRAN (R 3.5.1)
+#  devtools    * 2.0.1   2018-10-26 [1] CRAN (R 3.5.1)
+#  digest        0.6.18  2018-10-10 [1] CRAN (R 3.5.1)
+#  dplyr         0.7.8   2018-11-10 [1] CRAN (R 3.5.1)
+#  fs            1.2.6   2018-08-23 [2] CRAN (R 3.5.1)
+#  ggplot2     * 3.1.0   2018-10-25 [1] CRAN (R 3.5.1)
+#  glue          1.3.0   2018-07-17 [1] CRAN (R 3.5.1)
+#  gtable        0.2.0   2016-02-26 [2] CRAN (R 3.5.0)
+#  htmltools     0.3.6   2017-04-28 [2] CRAN (R 3.5.0)
+#  htmlwidgets   1.3     2018-09-30 [1] CRAN (R 3.5.1)
+#  httpuv        1.4.5.1 2018-12-18 [2] CRAN (R 3.5.1)
+#  labeling      0.3     2014-08-23 [2] CRAN (R 3.5.0)
+#  later         0.7.5   2018-09-18 [2] CRAN (R 3.5.1)
+#  lattice       0.20-38 2018-11-04 [3] CRAN (R 3.5.1)
+#  lazyeval      0.2.1   2017-10-29 [2] CRAN (R 3.5.0)
+#  magrittr      1.5     2014-11-22 [1] CRAN (R 3.5.0)
+#  memoise       1.1.0   2017-04-21 [2] CRAN (R 3.5.0)
+#  mime          0.6     2018-10-05 [1] CRAN (R 3.5.1)
+#  munsell       0.5.0   2018-06-12 [2] CRAN (R 3.5.0)
+#  pillar        1.3.1   2018-12-15 [1] CRAN (R 3.5.1)
+#  pkgbuild      1.0.2   2018-10-16 [2] CRAN (R 3.5.1)
+#  pkgconfig     2.0.2   2018-08-16 [1] CRAN (R 3.5.1)
+#  pkgload       1.0.2   2018-10-29 [2] CRAN (R 3.5.1)
+#  plyr          1.8.4   2016-06-08 [2] CRAN (R 3.5.0)
+#  png           0.1-7   2013-12-03 [2] CRAN (R 3.5.0)
+#  prettyunits   1.0.2   2015-07-13 [1] CRAN (R 3.5.0)
+#  processx      3.2.1   2018-12-05 [1] CRAN (R 3.5.1)
+#  promises      1.0.1   2018-04-13 [2] CRAN (R 3.5.0)
+#  ps            1.3.0   2018-12-21 [2] CRAN (R 3.5.1)
+#  purrr         0.2.5   2018-05-29 [2] CRAN (R 3.5.0)
+#  R6            2.3.0   2018-10-04 [2] CRAN (R 3.5.1)
+#  Rcpp          1.0.0   2018-11-07 [1] CRAN (R 3.5.1)
+#  remotes       2.0.2   2018-10-30 [1] CRAN (R 3.5.1)
+#  reshape2      1.4.3   2017-12-11 [2] CRAN (R 3.5.0)
+#  rlang         0.3.1   2019-01-08 [1] CRAN (R 3.5.1)
+#  rmote       * 0.3.4   2018-05-02 [1] deltarho (R 3.5.0)
+#  rprojroot     1.3-2   2018-01-03 [2] CRAN (R 3.5.0)
+#  scales        1.0.0   2018-08-09 [2] CRAN (R 3.5.1)
+#  servr         0.11    2018-10-23 [1] CRAN (R 3.5.1)
+#  sessioninfo   1.1.1   2018-11-05 [1] CRAN (R 3.5.1)
+#  stringi       1.2.4   2018-07-20 [2] CRAN (R 3.5.1)
+#  stringr       1.3.1   2018-05-10 [1] CRAN (R 3.5.0)
+#  testthat      2.0.1   2018-10-13 [1] CRAN (R 3.5.1)
+#  tibble        2.0.1   2019-01-12 [1] CRAN (R 3.5.1)
+#  tidyselect    0.2.5   2018-10-11 [2] CRAN (R 3.5.1)
+#  usethis     * 1.4.0   2018-08-14 [2] CRAN (R 3.5.1)
+#  withr         2.1.2   2018-03-15 [2] CRAN (R 3.5.0)
+#  xfun          0.4     2018-10-23 [1] CRAN (R 3.5.1)
+#
+# [1] /users/lcollado/R/x86_64-pc-linux-gnu-library/3.5.x
+# [2] /jhpce/shared/jhpce/core/conda/miniconda-3/envs/svnR-3.5.x/R/3.5.x/lib64/R/site-library
+# [3] /jhpce/shared/jhpce/core/conda/miniconda-3/envs/svnR-3.5.x/R/3.5.x/lib64/R/library
