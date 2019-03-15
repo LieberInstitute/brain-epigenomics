f6f98352450f180c203f228bb5a860ea60b840c4
aprice26 <amanda.joy.price@gmail.com>
2019-03-15 16:16:20 -0400

    change paths

--- LDSC/code/run_LDScore_estimation.R
+++ LDSC/code/run_LDScore_estimation.R
@@ -1,6 +1,9 @@
 # Run LD score estimation with functional categories (made in make-annot.R)
-# Peter Hickey
-# 2018-06-11
+# Code adapted from Peter Hickey (https://github.com/hansenlab/BrainEpigenomeNN/blob/master/SLDSR/scripts/run_LDScore_estimation.R)
+
+
+
+categories = readRDS("/dcl01/lieber/ajaffe/lab/brain-epigenomics/rdas/ldsc/categories.rds")
 
 # NOTE: On JHPCE, requires `module load python/2.7.6`
 
@@ -24,14 +27,14 @@ lapply(names(categories)[i], function(cn) {
   message(cn)
   mclapply(seqlevels, function(sl) {
     cmd <- paste0("python ",
-                  "/users/phickey/software/ldsc/ldsc.py ",
+                  "/users/aprice26/biotools/ldsc/ldsc.py ",
                   "--l2 ",
-                  "--bfile ../extdata/Phase1/1000G_plinkfiles/1000G.mac5eur.",
+                  "--bfile /dcl01/lieber/ajaffe/lab/brain-epigenomics/rdas/ldsc/Phase1/1000G_plinkfiles/1000G.mac5eur.",
                   sl, " ",
                   "--ld-wind-cm 1 ",
-                  "--annot ../output/ldsc/", cn, ".Phase1.", sl, ".annot.gz ",
-                  "--out ../output/ldsc/", cn, ".Phase1.", sl, " ",
-                  "--print-snps ../extdata/Phase1/hapmap3_snps/hm.", sl, ".snp")
+                  "--annot /dcl01/lieber/ajaffe/lab/brain-epigenomics/rdas/ldsc/annotation/", cn, ".Phase1.", sl, ".annot.gz ",
+                  "--out /dcl01/lieber/ajaffe/lab/brain-epigenomics/rdas/ldsc/", cn, ".Phase1.", sl, " ",
+                  "--print-snps /dcl01/lieber/ajaffe/lab/brain-epigenomics/rdas/ldsc/Phase1/hapmap3_snps/hm.", sl, ".snp")
     print(cmd)
     system(cmd)
   }, mc.cores = 10)
@@ -40,18 +43,17 @@ lapply(names(categories)[i], function(cn) {
 # ------------------------------------------------------------------------------
 # 'base' category
 #
+# Ran interactively
 
-# NOTE: Run this manually, if required
-# mclapply(seqlevels, function(sl) {
-#   cmd <- paste0("python ",
-#                 "/users/phickey/software/ldsc/ldsc.py ",
-#                 "--l2 ",
-#                 "--bfile ../extdata/Phase1/1000G_plinkfiles/1000G.mac5eur.",
-#                 sl, " ",
-#                 "--ld-wind-cm 1 ",
-#                 "--annot ../output/ldsc/base.Phase1.", sl, ".annot.gz ",
-#                 "--out ../output/ldsc/base.Phase1.", sl, " ",
-#                 "--print-snps ../extdata/Phase1/hapmap3_snps/hm.", sl, ".snp")
-#   print(cmd)
-#   system(cmd)
-# }, mc.cores = 10)
+#mclapply(seqlevels, function(sl) {
+# cmd <- paste0("python ",
+#               "/users/aprice26/biotools/ldsc/ldsc.py ",
+#               "--l2 ",
+#               "--bfile /dcl01/lieber/ajaffe/lab/brain-epigenomics/rdas/ldsc/Phase1/1000G_plinkfiles/1000G.mac5eur.", sl, " ",
+#               "--ld-wind-cm 1 ",
+#               "--annot /dcl01/lieber/ajaffe/lab/brain-epigenomics/rdas/ldsc/annotation/base.Phase1.", sl, ".annot.gz ",
+#               "--out /dcl01/lieber/ajaffe/lab/brain-epigenomics/rdas/ldsc/base.Phase1.", sl, " ",
+#               "--print-snps /dcl01/lieber/ajaffe/lab/brain-epigenomics/rdas/ldsc/Phase1/hapmap3_snps/hm.", sl, ".snp")
+# print(cmd)
+# system(cmd)
+#}, mc.cores = 10)
