c572f69ea3359ca38990ae5f13169168e41c6943
aprice26 <amanda.joy.price@gmail.com>
2019-03-15 16:16:03 -0400

    change features to reflect our features

--- LDSC/code/make-annot.R
+++ LDSC/code/make-annot.R
@@ -5,6 +5,8 @@
 ### NOTEs
 ###
 
+# qrsh -l bluejay,mem_free=100G,h_vmem=100G,h_fsize=100G
+
 # - Adapted from tutorial at https://github.com/bulik/ldsc/wiki/LD-Score-Estimation-Tutorial#building-on-top-of-the-finucane-et-al-baseline-model
 
 ### ============================================================================
@@ -16,6 +18,7 @@ library(GenomicRanges)
 library(R.utils)
 library(rtracklayer)
 library(AnnotationHub)
+library(bumphunter)
 
 options("mc.cores" = 40)
 
@@ -26,88 +29,87 @@ seqlevels <- 1:22
 ###
 
 # ------------------------------------------------------------------------------
-# Our data
+# Not separated by sign
 #
 
-load("../../integrating-dmrs-dars-and-degs/objects/assays-and-features.rda")
-load("../../Objects/All_Annotated_DMRs_GRanges.rda")
-load("../../Objects/All_BLOCK_POS_DMRs_fwer50.rda")
-load("../../Objects/Annotated_POSvNEG_BLOCKs_GRanges.rda")
+load("/dcl01/lieber/ajaffe/lab/brain-epigenomics/bumphunting/rda/limma_Neuron_CpGs_minCov_3_ageInfo_dmrs.Rdata")
+load("/dcl01/lieber/ajaffe/lab/brain-epigenomics/rdas/DMR/DMR_objects.rda")
+load('/dcl01/lieber/ajaffe/lab/brain-epigenomics/bumphunting/BSobj_bsseqSmooth_Neuron_minCov_3.Rdata')
 
-# Non-differential features
-OCRs <- list(
-  "NAcc_pos_OCRs" = ocrs_NAcc_pos,
-  "NAcc_neg_OCRs" = ocrs_NAcc_neg,
-  "BA9_pos_OCRs" = ocrs_BA9_pos,
-  "BA9_neg_OCRs" = ocrs_BA9_neg,
-  "overall_OCRs" = ocrs_overall)
-non_DARs <- subsetByOverlaps(ocrs_overall,
-                             c(dars_pos, dars_neg, dars_pos_vs_neg),
-                             invert = TRUE)
+# Identify all CpG clusters in the genome
+
+gr = granges(BSobj)
+cl = clusterMaker( chr = as.character(seqnames(gr)), pos = start(gr),   maxGap = 1000)
+gr.clusters = split(gr, cl)
+gr.clusters = unlist(range(gr.clusters))
+df.clusters = as.data.frame(gr.clusters)
+df.clusters$regionID = paste0(df.clusters$seqnames,":",df.clusters$start,"-",df.clusters$end)
+df.clusters$rnum = 1:length(gr.clusters)
+
+DMRs <- c(list(
+  "Celltype_HypoNeuron" = unique(makeGRangesFromDataFrame(DMR$CellType[which(DMR$CellType$value<0 & DMR$CellType$sig=="FWER < 0.05"),])),
+  "Celltype_HypoGlia" = unique(makeGRangesFromDataFrame(DMR$CellType[which(DMR$CellType$value>0 & DMR$CellType$sig=="FWER < 0.05"),])),
+  "Age_Decreasing" = unique(makeGRangesFromDataFrame(DMR$Age[which(DMR$Age$value<0 & DMR$Age$sig=="FWER < 0.05"),])), 
+  "Age_Increasing" = unique(makeGRangesFromDataFrame(DMR$Age[which(DMR$Age$value>0 & DMR$Age$sig=="FWER < 0.05"),]))),
+  as.list(split(dmrs, dmrs$k6cluster_label)))
+names(DMRs)[5:10] = c("Gr1_cdDMRs", "Gr2_cdDMRs", "Gr3_cdDMRs", "Gr4_cdDMRs", "Gr5_cdDMRs", "Gr6_cdDMRs")
+  
+non_DMRs <- subsetByOverlaps(gr.clusters, unlist(as(DMRs, "GRangesList")), invert = TRUE)
+
 set.seed(3000)
-OCR_subsets <- mclapply(
-  list("POS_non-DARs" = dars_pos, "POSvsNEG_non-DARs" = dars_pos_vs_neg),
-  function(dars) {
+
+nonDMR_subsets <- mclapply(
+  list(Celltype_HypoNeuron_nonDMRs = DMRs$Celltype_HypoNeuron, Celltype_HypoGlia_nonDMRs = DMRs$Celltype_HypoGlia, 
+       Age_Decreasing_nonDMRs = DMRs$Age_Decreasing, Age_Increasing_nonDMR = DMRs$Age_Increasing,
+       Gr1_nonDMRs = DMRs$Gr1_cdDMRs, Gr2_nonDMRs = DMRs$Gr2_cdDMRs, Gr3_nonDMRs = DMRs$Gr3_cdDMRs, 
+       Gr4_nonDMRs = DMRs$Gr4_cdDMRs, Gr5_nonDMRs = DMRs$Gr5_cdDMRs, Gr6_nonDMRs = DMRs$Gr6_cdDMRs),
+  function(drs) {
     # Keep sampling until get non-DARs with about the same total width as DARs
-    tmp <- sample(non_DARs, length(dars))
-    while (sum(width(tmp)) < sum(width(dars))) {
-      message(sum(width(dars)) - sum(width(tmp)))
+    tmp <- sample(non_DMRs, length(drs))
+    while (sum(width(tmp)) < sum(width(drs))) {
+      message(sum(width(drs)) - sum(width(tmp)))
       tmp <- c(tmp, sample(
-        subsetByOverlaps(non_DARs, tmp, invert = TRUE, type = "equal"),
+        subsetByOverlaps(non_DMRs, tmp, invert = TRUE, type = "equal"),
         1000))
     }
-    list("sameN" = sample(non_DARs, length(dars)),
+    list("sameN" = sample(non_DMRs, length(drs)),
          "sameWidth" = tmp)
   }, mc.cores = getOption("mc.cores"))
-our_non_differential_features <- c(OCRs, unlist(OCR_subsets, recursive = FALSE))
+
+our_differential_features <- c(DMRs, unlist(nonDMR_subsets, recursive = FALSE), list(non_DMRs = non_DMRs))
 # NOTE: Rename to avoid '.' in names
-names(our_non_differential_features) <-
-  gsub("\\.", "_", names(our_non_differential_features))
+names(our_differential_features) <- gsub("\\.", "_", names(our_differential_features))
 
-# Differential features
-POS_CG_DMRs <- Annotated_POS_DMRs_gr
-POSvsNEG_CG_DMRs <- Annotated_POSvNEG_DMRs_gr
-POS_CG_blocks <- makeGRangesFromDataFrame(sig_block_dmrs,
-                                          keep.extra.columns = TRUE)
-POSvsNEG_CG_blocks <- Annotated_POSvNEG_BLOCKs_gr
-POS_DARs <- dars_pos
-POS_bigDARs <- dars_pos[abs(dars_pos$logFC) >= 1]
-POSvsNEG_DARs <- dars_pos_vs_neg
-POSvsNEG_bigDARs <- POSvsNEG_DARs[abs(POSvsNEG_DARs$logFC) >= 1]
-list_of_candidate_CH_DMRs <-
-  readRDS("../../nonCG/objects/list_of_candidate_CH_DMRs.with_meanMeth.rds")
-list_of_CH_DMRs <- endoapply(list_of_candidate_CH_DMRs, function(dmrs) {
-  dmrs[dmrs$fwer / dmrs$successful_permutations <= 0.05]
-})
-CH_DMRs <- reduce(unlist(GRangesList(list_of_CH_DMRs)), ignore.strand = TRUE)
-
-our_differential_features <- list(
-  `POS_CG-DMRs` = POS_CG_DMRs,
-  `POSvsNEG_CG-DMRs` = POSvsNEG_CG_DMRs,
-  `POS_CG-blocks` = POS_CG_blocks,
-  `POSvsNEG_CG-blocks` = POSvsNEG_CG_blocks,
-  POS_DARs = POS_DARs,
-  POS_bigDARs = POS_bigDARs,
-  POSvsNEG_DARs = POSvsNEG_DARs,
-  POSvsNEG_bigDARs = POSvsNEG_bigDARs,
-  `CH-DMRs` = CH_DMRs,
-  `POS_CG-DMRs_union_POS_bigDARs` = union(POS_CG_DMRs, POS_bigDARs)
-)
 
 # ------------------------------------------------------------------------------
-# Other people's data
+# Non-differential features
 #
 
-Luo_small_CG_DMRs <- readRDS("../../Objects/Luo_small_CG_DMRs.rds")
-load("../../Objects/Lister_hs_CGDMRs.rda")
+load("/dcl01/lieber/ajaffe/lab/brain-epigenomics/rdas/CREs/UMRs_LMRs_methylSeekR.rda")
+load("/dcl01/lieber/ajaffe/lab/brain-epigenomics/rdas/CREs/UMRs_LMRs_methylSeekR_prenatal.rda")
 
-# Non-differential features
-brain_enhancers <- unlinked_enhancers["H3K27ac"]
+rm("UMRLMRsegments.CG", "UMRLMRsegments.CGpren")
+
+
+# Non-differential low-methylated regions (LMRs)
+
+LMRs <- list(Prenatal_LMRs = lapply(UMRLMRsegments.CG.100kb.pren, function(y) y[y$type=="LMR"]),
+             Neuronal_LMRs = lapply(UMRLMRsegments.CG.100kb[which(names(UMRLMRsegments.CG.100kb) %in% pd[which(pd$Cell.Type=="Neuron"),"Data.ID"])],
+                                    function(y) y[y$type=="LMR"]),
+             Glial_LMRs = lapply(UMRLMRsegments.CG.100kb[which(names(UMRLMRsegments.CG.100kb) %in% pd[which(pd$Cell.Type=="Glia"),"Data.ID"])],
+                                 function(y) y[y$type=="LMR"]))
+LMRs <- lapply(LMRs, function(x) reduce(unlist(as(x, "GRangesList"))))
+
+non_dLMRs <- lapply(LMRs, function(gr) subsetByOverlaps(gr, unlist(as(DMRs, "GRangesList")), invert = TRUE))
+
 
 # Subset of chromHMM track with regulatory region-like states
+
 ah <- AnnotationHub()
+
 # Ten brain samples with chromHMM from RoadMap; identified using
 # query(ah, c("chromHMM", "brain"))
+
 list_of_chromHMM <- ah[c("AH46920", "AH46921", "AH46922", "AH46923", "AH46924",
                          "AH46925", "AH46926", "AH46927", "AH46934", "AH46935")]
 chromHMM_regulatory_regions <- lapply(list_of_chromHMM, function(x) {
@@ -120,57 +122,10 @@ chromHMM_regulatory_regions <- lapply(list_of_chromHMM, function(x) {
 
 chromHMM_regulatory_regions <- list(
   "chromHMM_union" = reduce(unlist(GRangesList(chromHMM_regulatory_regions))))
-others_non_differential_features <- c(brain_enhancers,
-                                      chromHMM_regulatory_regions)
 
-# Differential features
-Lister_CG_DMRs <- makeGRangesFromDataFrame(Lister_DMRs,
-                                           keep.extra.columns = TRUE)
-Lister_CG_DMRs <-
-  sort(sortSeqlevels(keepSeqlevels(Lister_CG_DMRs, paste0("chr", seqlevels),
-                                   pruning.mode = "coarse")))
-# NOTE: Lister's CG-DMRs form a motley crew of 4 sets (from the Supplementary
-#       Material):
-#       - NeuN+ hyper-mCG: This set includes CG-DMRs where mCG/CG is larger in
-#         NeuN+ compared with NeuN– and Fetal; larger than Fetal only; or
-#         larger than NeuN– only
-#       - NeuN– hyper-mCG: This set includes CG-DMRs where mCG/CG is larger in
-#         NeuN– compared with NeuN+, and those in which mCG/CG is larger in
-#         NeuN– than in fetal.
-#       - NeuN+ hypo-mCG: This set includes CG-DMRs where mCG/CG is smaller in
-#         NeuN+ compared with NeuN– and Fetal; or smaller compared with Fetal
-#         only.
-#       - NeuN– hypo-mCG: This set includes CG-DMRs where mCG/CG is smaller in
-#         NeuN– compared with NeuN+ and Fetal, or Fetal only.
-#       Notably, CG-DMRs may overlap one another, so we merge to form disjoint
-#       regions
-Lister_CG_DMRs <- disjoin(Lister_CG_DMRs)
-set.seed(7000)
-CG_DMR_subsets <- mcmapply(
-  function(others_dmrs, our_dmrs) {
-    # Keep sampling until get others DMRs with about the same total width as
-    # our DMRs
-    tmp <- sample(others_dmrs, length(our_dmrs))
-    while (sum(width(tmp)) < sum(width(our_dmrs))) {
-      message(sum(width(our_dmrs)) - sum(width(tmp)))
-      tmp <- c(tmp, sample(
-        subsetByOverlaps(others_dmrs, tmp, invert = TRUE, type = "equal"),
-        1000))
-    }
-    list("sameN" = sample(others_dmrs, length(our_dmrs)),
-         "sameWidth" = tmp)
-  }, others_dmrs = list("Luo_small_CG-DMRs" = Luo_small_CG_DMRs,
-                        "Lister_CG-DMRs" = Lister_CG_DMRs),
-  our_dmrs = list("POS_CG-DMRs" = POS_CG_DMRs,
-                  "POSvsNEG_CG-DMRs" = POSvsNEG_CG_DMRs),
-  SIMPLIFY = FALSE,
-  mc.cores = getOption("mc.cores"))
-others_differential_features <-
-  c(list("Luo_small_CG-DMRs" = Luo_small_CG_DMRs,
-         "Lister_CG-DMRs" = Lister_CG_DMRs))
-# NOTE: Rename to avoid '.' in names
-names(others_differential_features) <-
-  gsub("\\.", "_", names(others_differential_features))
+non_differential_features <- c(non_dLMRs,
+                               chromHMM_regulatory_regions)
+
 
 # ------------------------------------------------------------------------------
 # CNS (central nervous system) cell type group
@@ -180,7 +135,7 @@ names(others_differential_features) <-
 #       However, the BED files were not, so I construct an approximation to the
 #       BED files based on the annotation files
 CNS_annot <- mclapply(seqlevels, function(sl) {
-  read_tsv(file.path(extdir, "Phase1", "cell_type_groups",
+  read_tsv(file.path("/dcl01/lieber/ajaffe/lab/brain-epigenomics/rdas/ldsc/Phase1", "cell_type_groups",
                      paste0("CNS.", sl, ".annot.gz")))
 }, mc.cores = getOption("mc.cores"))
 CNS_BED <- unlist(GRangesList(mclapply(CNS_annot, function(x) {
@@ -197,15 +152,13 @@ CNS_BED <- unlist(GRangesList(mclapply(CNS_annot, function(x) {
 # List of all brain-derived features
 #
 
-categories <- c(our_non_differential_features,
-                our_differential_features,
-                others_non_differential_features,
-                others_differential_features,
+categories <- c(our_differential_features,
+                non_differential_features,
                 list("CNS" = CNS_BED))
 stopifnot(all(sapply(categories, isDisjoint)))
 stopifnot(all(!grepl("\\.", names(categories))))
 
-saveRDS(categories, "../objects/categories.rds")
+saveRDS(categories, "/dcl01/lieber/ajaffe/lab/brain-epigenomics/rdas/ldsc/categories.rds")
 
 ### ============================================================================
 ### Make annotations
@@ -216,7 +169,7 @@ k <- grep("CNS", names(categories), invert = TRUE)
 mclapply(seqlevels, function(sl) {
   message(sl)
   cds <- read_tsv(
-    paste0("../extdata/Phase1/cell_type_groups/CNS.", sl,
+    paste0("/dcl01/lieber/ajaffe/lab/brain-epigenomics/rdas/ldsc/Phase1/cell_type_groups/CNS.", sl,
            ".annot.gz"))
   cds_gr <- GRanges(paste0("chr", cds$CHR), IRanges(cds$BP, width = 1L))
   annot <- cds[, c("CHR", "BP", "SNP", "CM")]
@@ -226,11 +179,12 @@ mclapply(seqlevels, function(sl) {
   }, mc.cores = 4)
   # 'Marginal' annotation file
   mclapply(names(categories[k]), function(n) {
-    fl <- paste0("../output/ldsc/", n, ".Phase1.", sl, ".annot")
+    fl <- paste0("/dcl01/lieber/ajaffe/lab/brain-epigenomics/rdas/ldsc/annotation/", n, ".Phase1.", sl, ".annot")
     write_tsv(annot[, c("CHR", "BP", "SNP", "CM", n)], fl)
     gzip(fl, overwrite = TRUE)
   }, mc.cores = 4)
 }, mc.cores = 10)
+
 
 # ------------------------------------------------------------------------------
 # Annot file for 'base' category
@@ -238,9 +192,9 @@ mclapply(seqlevels, function(sl) {
 #
 
 mclapply(seqlevels, function(sl) {
-  x <- read_tsv(file.path(extdir, "Phase1", "baseline",
+  x <- read_tsv(file.path("/dcl01/lieber/ajaffe/lab/brain-epigenomics/rdas/ldsc/", "Phase1", "baseline",
                           paste0("baseline.", sl, ".annot.gz")))
-  write_tsv(x[, 1:5], paste0("../output/ldsc/base.Phase1.", sl, ".annot.gz"))
+  write_tsv(x[, 1:5], paste0("/dcl01/lieber/ajaffe/lab/brain-epigenomics/rdas/ldsc/annotation/base.Phase1.", sl, ".annot.gz"))
 }, mc.cores = getOption("mc.cores"))
 
 ### ============================================================================
