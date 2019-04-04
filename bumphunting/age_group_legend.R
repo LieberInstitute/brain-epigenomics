d735a7228bfc288055ef9dedecc18f2dea2110a7
aprice26 <amanda.joy.price@gmail.com>
2019-04-04 15:52:41 -0400

    update expression figures

--- bumphunting/age_group_legend.R
+++ bumphunting/age_group_legend.R
@@ -7,3 +7,8 @@ labs <- c(paste(c('Glia:', 'Neuron:'), rep(c('Infant', 'Child', 'Teen', 'Adult')
 cols <- c(brewer.pal(8, "Paired")[c(5:6, 7:8, 3:4, 1:2)], 'grey50')
 legend('top', labs[c(1, 3, 5, 7, 9, 2, 4, 6, 8)], col = cols[c(1, 3, 5, 7, 9, 2, 4, 6, 8)], lwd = 3, bty = 'n', ncol = 2)
 dev.off()
+
+pdf('cell_types.pdf', useDingbats = FALSE)
+plot.new()
+legend(x = 'top', c("Glia", "Neuron"), col = brewer.pal(name="Dark2", n=2), lwd = 3, bty = 'n', ncol = 2)
+dev.off()
